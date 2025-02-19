6c942b7b6962e93c2f91effd9579c5d0c0c90108
xtrel <hubgon2007@proton.me>
2025-02-19 14:39:28 +0100

    Working protoytpe
    TO DO:
    * make documentation for devs and for users
    * fill out README
    * maybe dont use goto

--- world2pixel/source/main.cpp
+++ world2pixel/source/main.cpp
@@ -1,22 +1,92 @@
 #include <geodesk/geodesk.h>
+#include <stdlib.h>
 
 using namespace geodesk;
 
+//latitude - north south
+//longitude - east west
+//Yea its easy to mess it up sometimes
+
+/*
+TO DO:
+* make documentation for devs and for users
+* fill out README
+* maybe dont use goto
+*/
+
 int main(int argc, char* argv[])
 {
-    //latitude - north south
-    //longitude - east west
-    Features features("gol");
-    Coordinate myLocation = Coordinate::ofLonLat(17.225415,51.662171);
-    Features inradius = features.maxMetersFrom(10, myLocation);
+
+    for(int i=0;i<argc;i++)
+    {
+        std::cout<<"Command parameter "<<i<<": "<<argv[i]<<"\n";
+    }
+
+    float targetlatitude,targetlongitude,targetradius;
+    targetlatitude = atof(argv[1]);
+    targetlongitude = atof(argv[2]);
+    targetradius = atof(argv[3]);
+
+    std::string filename = "gol";
+    if(argc>4) 
+    {
+        filename = argv[4];
+    }
+
+    Features features(filename.c_str());
+    Features inradius = features.maxMetersFromLonLat(targetradius, targetlongitude, targetlatitude);
     for (Feature curitem: inradius)
     {
-        std::cout << curitem["name"]<<" "<< curitem.id()<<" "<<curitem.typeName() << std::endl;
+        Tags tags = curitem.tags();
+        bool typeout = false;
+        std::vector<std::string> allowedkeys = {"landuse","natural","building","railway","highway","power","waterway"};
+        
+        for(Tag curtag: tags)
+        {
+            for(int i = 0; i < allowedkeys.size(); i++)
+            {
+                if(curtag.key() == allowedkeys[i])
+                {
+                    typeout = true;
+                    goto skiptags;
+                }
+            }
+        }
+
+        skiptags:
+
+        if(typeout)
+        {
+            std::cout << curitem["name"]<<" "<< curitem.id()<<" "<<curitem.typeName() << std::endl;
+            for(Tag curtag: tags)
+            {
+                std::string key = curtag.key();
+                std::string value = curtag.value();
+                std::string curtagstring = key + "=" + value;
+                for(int i = 0; i < allowedkeys.size(); i++)
+                {
+                    if(curtag.key() == allowedkeys[i])
+                    {
+                        std::cout<<"        ## "<<curtagstring<<std::endl;  
+                    }
+                }
+            }
+        }
     }
 }
 
+/*
+SONGS THAT YOUTUBE PLAYED WHILE DEVVING
+2025.02.19
+14:20
+https://www.youtube.com/watch?v=Hd2-Akz6Ht4
+14:30
+https://www.youtube.com/watch?v=0JliRJNGwQU
+*/
+
 /*THE GRAVEYARD*/
 /*Rest in peace code prototypes [*]*/
+
 /*
     Box bounds = Box::ofWSEN(
     cords);
